6ac9dbdb1e31a47c662711b20deb4c068e6ddbe8
ArtemGrinn <ahaletskiy@gmail.com>
2016-08-09 23:33:57 +0300

    categories

--- js/app.js
+++ js/app.js
@@ -1,14 +1,21 @@
 (function(){
   "use strict";
+  
+  var data = {};
+  var contentContainer = document.querySelector('.content');
 
   window.onload = function(){
-    var container = document.querySelector('.content');
-    sendRequest('http://helenj.myjino.ru/meals.js', 'GET', function(response){
-      localStorage.setItem('data', response);
-      var data = JSON.parse(localStorage.getItem('data'));
-      container.innerHTML = '';
-      container.appendChild(createCategories(data.categories));
-    });
+    sendRequest('http://helenj.myjino.ru/meals.js', 'GET', main);
+  }
+
+  function main(response){
+      data = JSON.parse(response);
+
+      var categories = document.querySelector('.categories');
+      categories.innerHTML = '';
+      categories.appendChild(createCategories(data.categories));
+
+      
   }
 
   function createCategories(ctgs){
@@ -20,10 +27,30 @@
       var link = document.createElement('a');
       link.href='';
       link.innerHTML = 'Выбрать';
+      link.addEventListener('click', function(e){
+        e.preventDefault();
+        showCategory(ctg.id);
+      });
       wrapper.appendChild(label).appendChild(link);
       categories.appendChild(wrapper);
     });
     return categories;
+  }
+
+  function showCategory(id){
+    contentContainer.innerHTML = '';
+    var filtered = data.meals.filter(function(meal){
+      return meal.category_id == id;
+    }).slice(0, 20);
+    
+    [].forEach.call(filtered, function(item){
+      var wrapper = document.createElement('div');
+      var a = document.createElement('a');
+      a.href='';
+      a.innerHTML = item.name;
+      wrapper.appendChild(a); 
+      contentContainer.appendChild(wrapper);
+    });
   }
 
   function sendRequest(url, method, callback){
