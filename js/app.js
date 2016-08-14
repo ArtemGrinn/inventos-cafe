99ae4084b20f4bb88fe71bac32ca36839f428312
ArtemGrinn <ahaletskiy@gmail.com>
2016-08-14 19:29:41 +0300

    app.js and styles

--- js/app.js
+++ js/app.js
@@ -1,54 +1,121 @@
 (function(){
   "use strict";
   
-  var data = {};
+  var Data = {};
+  var Order = {};
   var contentContainer = document.querySelector('.content');
-  var categoriesContainer  = document.querySelector('.categories');
 
   window.onload = function(){
     sendRequest('http://helenj.myjino.ru/meals.js', 'GET', main);
   }
 
   function main(response){
-      data = JSON.parse(response);
-      
-      categoriesContainer.innerHTML = '';
-      categoriesContainer.appendChild(createCategories(data.categories));
+      Data = JSON.parse(response);
+      loadOrderFromStorage();
+      createCategories();
   }
 
-  function createCategories(ctgs){
-    var categories = document.createElement('div');
-    [].forEach.call(ctgs, function(ctg){
-      var wrapper = document.createElement('div');
+  function createCategories(){
+    contentContainer.innerHTML = '';
+    [].forEach.call(Data.categories, function(category){
+      var wrapper = document.createElement('div');     
       var label = document.createElement('span');
-      label.innerHTML = ctg.name;
+      label.className = 'label';
+      label.innerHTML = category.name + ":";
+      wrapper.appendChild(label);
+
+      if(Order[category.id]){
+        var selectedMeal = document.createElement('span');
+        selectedMeal.className = 'selected-meal';
+        selectedMeal.innerHTML = Order[category.id].name;
+        wrapper.appendChild(selectedMeal);
+      }
+
       var link = document.createElement('a');
       link.href='';
-      link.innerHTML = 'Выбрать';
+      link.innerHTML = Order[category.id] ? 'Изменить': 'Выбрать';
       link.addEventListener('click', function(e){
         e.preventDefault();
-        showCategory(ctg.id);
-      });
-      wrapper.appendChild(label).appendChild(link);
-      categories.appendChild(wrapper);
-    });
-    return categories;
+        showMeals(category.id);
+      });   
+      wrapper.appendChild(link);
+      contentContainer.appendChild(wrapper);
+    });    
+    contentContainer.appendChild(createNextButton());
   }
 
-  function showCategory(id){
+  function createNextButton(){
+    var button = document.createElement('button');
+    button.innerHTML = 'Далее';
+    button.className = 'next';
+    return button;
+  }
+
+  function showMeals(id){
     contentContainer.innerHTML = '';
-    var filtered = data.meals.filter(function(meal){
+    var filtered = Data.meals.filter(function(meal){
       return meal.category_id == id;
     }).slice(0, 20);
     
     [].forEach.call(filtered, function(item){
-      var wrapper = document.createElement('div');
-      var a = document.createElement('a');
-      a.href='';
-      a.innerHTML = item.name;
-      wrapper.appendChild(a); 
-      contentContainer.appendChild(wrapper);
+      var mealElement = createMeal(item);
+      contentContainer.appendChild(mealElement);
     });
+  }
+
+  function createMeal(mealParams){
+      var wrapper = document.createElement('div');
+      wrapper.setAttribute('data-id', mealParams.id);
+      wrapper.classList.add('meal');
+
+      var header = document.createElement('header');
+      header.innerHTML = mealParams.name;
+   
+      var video = document.createElement('video');       
+      if(mealParams.video){
+        var player = dashjs.MediaPlayer().create();
+        player.initialize(video, mealParams.video, false);
+        //player.src({
+        //  src: mealParams.video,
+        //  type: 'application/x-mpegURL'
+        //});  
+        video.addEventListener('click', function(e){       
+          //player.isPaused() ? player.play() : player.pause();
+          player.play();
+        });
+      }
+
+      var price = document.createElement('div');
+      price.classList.add('price');
+      price.innerHTML = mealParams.price + ' руб.';
+
+      var a = document.createElement('a');
+      a.href='#';
+      a.innerHTML = 'Выбрать';
+      a.addEventListener('click', function(e){
+        e.preventDefault();
+        selectMeal(mealParams);
+      });
+
+      wrapper.appendChild(header);
+      wrapper.appendChild(video);
+      wrapper.appendChild(price);
+      wrapper.appendChild(a);
+      return wrapper;
+  }
+
+  function selectMeal(mealData){   
+    Order[mealData.category_id] = mealData;
+    saveOrderToStorage();
+    contentContainer.innerHTML = '';
+    createCategories(); 
+  }
+
+  function saveOrderToStorage(){  
+    localStorage.setItem('inventos-kafe-order', JSON.stringify(Order));
+  }
+  function loadOrderFromStorage(){     
+    Order = JSON.parse(localStorage.getItem('inventos-kafe-order')) || {};
   }
 
   function sendRequest(url, method, callback){
