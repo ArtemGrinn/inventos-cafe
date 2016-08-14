8cf170577563d4aa6d3981c92b64be378b2a7292
ArtemGrinn <ahaletskiy@gmail.com>
2016-08-14 23:37:03 +0300

    account page

--- js/app.js
+++ js/app.js
@@ -12,23 +12,31 @@
   function main(response){
       Data = JSON.parse(response);
       loadOrderFromStorage();
-      createCategories();
+      showCategories();
   }
 
-  function createCategories(){
+  function showCategories(){
     contentContainer.innerHTML = '';
     [].forEach.call(Data.categories, function(category){
       var wrapper = document.createElement('div');     
       var label = document.createElement('span');
-      label.className = 'label';
+      label.className = 'category__label';
       label.innerHTML = category.name + ":";
       wrapper.appendChild(label);
 
       if(Order[category.id]){
         var selectedMeal = document.createElement('span');
-        selectedMeal.className = 'selected-meal';
-        selectedMeal.innerHTML = Order[category.id].name;
+        selectedMeal.className = 'category__meal';
+        selectedMeal.innerHTML = Order[category.id].name + ' ' + Order[category.id].price + ' р';
         wrapper.appendChild(selectedMeal);
+
+        var removeLink = document.createElement('a');
+        removeLink.href='';
+        removeLink.innerHTML = 'Удалить';
+        removeLink.addEventListener('click', function(e){
+          e.preventDefault();
+          removeMeal(category.id);
+        });   
       }
 
       var link = document.createElement('a');
@@ -39,24 +47,59 @@
         showMeals(category.id);
       });   
       wrapper.appendChild(link);
+
+      removeLink &&
+      wrapper.appendChild(removeLink);
       contentContainer.appendChild(wrapper);
     });    
+    
+    contentContainer.appendChild(createTotalSum());
     contentContainer.appendChild(createNextButton());
   }
 
   function createNextButton(){
-    var button = document.createElement('button');
-    button.innerHTML = 'Далее';
-    button.className = 'next';
-    return button;
+    var a = document.createElement('a');
+    a.innerHTML = 'Далее';
+    a.className = 'next';
+    a.href = 'account.html';
+    return a;
+  }
+
+  function createTotalSum(){
+    var total = document.createElement('div');
+    total.className = 'total';
+    var sum = 0;
+    [].forEach.call(Data.categories, function(category){
+      if(Order[category.id]){
+        sum += Order[category.id].price;
+      }
+    });
+    var label = document.createElement('span');
+    label.className = 'category__label';
+    label.innerHTML = 'Итого:';
+    var totalSum = document.createElement('span');
+    totalSum.innerHTML = sum + ' Р';
+    total.appendChild(label);
+    total.appendChild(totalSum);
+    return total;
   }
 
   function showMeals(id){
     contentContainer.innerHTML = '';
     var filtered = Data.meals.filter(function(meal){
       return meal.category_id == id;
-    }).slice(0, 20);
+    });
     
+    var backBtn = document.createElement('a');
+    backBtn.className = 'back-btn';
+    backBtn.href='#';
+    backBtn.innerHTML = 'Назад';
+    backBtn.addEventListener('click', function(e){
+      e.preventDefault();
+      showCategories();
+    });
+    contentContainer.appendChild(backBtn);
+
     [].forEach.call(filtered, function(item){
       var mealElement = createMeal(item);
       contentContainer.appendChild(mealElement);
@@ -69,27 +112,30 @@
       wrapper.classList.add('meal');
 
       var header = document.createElement('header');
+      header.className = 'meal__header';
       header.innerHTML = mealParams.name;
    
-      var video = document.createElement('video');       
+      var video = document.createElement('video'); 
+      video.className = 'meal__video';      
       if(mealParams.video){
-        var player = dashjs.MediaPlayer().create();
-        player.initialize(video, mealParams.video, false);
+        //var player = dashjs.MediaPlayer().create();
+        //player.initialize(video, mealParams.video, false);
         //player.src({
         //  src: mealParams.video,
         //  type: 'application/x-mpegURL'
         //});  
         video.addEventListener('click', function(e){       
           //player.isPaused() ? player.play() : player.pause();
-          player.play();
+          //player.play();
         });
       }
 
       var price = document.createElement('div');
-      price.classList.add('price');
+      price.className = 'meal__price';
       price.innerHTML = mealParams.price + ' руб.';
 
       var a = document.createElement('a');
+      a.className = 'meal__link';
       a.href='#';
       a.innerHTML = 'Выбрать';
       a.addEventListener('click', function(e){
@@ -107,8 +153,13 @@
   function selectMeal(mealData){   
     Order[mealData.category_id] = mealData;
     saveOrderToStorage();
-    contentContainer.innerHTML = '';
-    createCategories(); 
+    showCategories(); 
+  }
+
+  function removeMeal(category_id){
+    delete Order[category_id];
+    saveOrderToStorage();
+    showCategories(); 
   }
 
   function saveOrderToStorage(){  
